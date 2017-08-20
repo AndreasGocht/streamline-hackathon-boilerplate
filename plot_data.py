38d3a5d8fa99c11a8abee1221dcef8d70b7ed469
Andreas Gocht <andreas.gocht@tu-dresden.de>
2017-08-20 12:06:15 +0200

    fix script

--- plot_data.py
+++ plot_data.py
@@ -1,7 +1,9 @@
 import argparse
 import os
+import os.path
 from matplotlib import pyplot as pp
 import time
+import re
 
 parser = argparse.ArgumentParser(description='shows events.')
 parser.add_argument('-p','--path', help = "path that holds the data")
@@ -9,26 +11,26 @@ parser.add_argument('-p','--path', help = "path that holds the data")
 args = parser.parse_args()
 
 fig = pp.figure()
-pp.show()
+
 
 while True:
     data = {}
 
-    for p in sorted(os.listdir(args.path)):
-        print(p)
-        filename = args.path + "/" + p+"/part-00001"
-        while not os.path.isfile(filename):
-            time.sleep(0.1)
-        with open(filename) as f:
-            for line in f:
-                line = line.strip("\n")
-                dat = line.split(",")
-                print(dat[0])
-                print("---")
-                print(data.get(dat[0],[]))
-                if dat[0] in data:
-                    print("hi")
-                data.setdefault(dat[0],[]).append(float(dat[1]))
+    for p in sorted(os.walk(args.path)):
+        if "_SUCCESS" in p[2]:
+            for filename in p[2]:
+                if re.match("part-[0-9]*$",filename):
+                    filepath = os.path.join(p[0],filename)
+                    with open(filepath) as f:
+                        for line in f:
+                            line = line.strip("\n")
+                            dat = line.split(",")
+                            print(dat[0])
+                            print("---")
+                            print(data.get(dat[0],[]))
+                            if dat[0] in data:
+                                print("hi")
+                            data.setdefault(dat[0],[]).append(float(dat[1]))
                 
     fig.clf()
     for key,val in data.items():
